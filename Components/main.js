4877120bea7720aa012d640de7d9362f44846a87
Mohamad Youness <pixlerfrost@gmail.com>
2022-03-11 22:33:26 -0500

    .

--- Components/main.js
+++ Components/main.js
@@ -1,24 +1,26 @@
 import { Box, Container } from '@chakra-ui/react'
 import Head from 'next/head'
 import Navbar from '../Components/navbar'
+import Earth3d from '../Components/earth-3d'
 
-const Main = ({router, children}) => {
- return (
-  <Box as='main' pb={8}>
-   <Head>
-    <meta name="viewport" content="width=device-width, initial-scale=1 "/>
-    <title>Mohamad Youness - HomePage</title>
-    <meta name="description" content="Mohamad's homepage" />
-    <link rel="shortcut icon" href="/images/logo.png" type="image/x-icon" />
-   </Head>
+const Main = ({ router, children }) => {
+  return (
+    <Box as='main' pb={8}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1 " />
+        <title>Mohamad Youness - HomePage</title>
+        <meta name="description" content="Mohamad's homepage" />
+        <link rel="shortcut icon" href="/images/logo.png" type="image/x-icon" />
+      </Head>
 
-   <Navbar path={router.asPath} />
+      <Navbar path={router.asPath} />
 
-   <Container maxW="container.md" pt={14}>
-    {children}
-    </Container>
-   </Box>
- )
+      <Container maxW="container.md" pt={14}>
+        {children}
+        <Earth3d />
+      </Container>
+    </Box>
+  )
 }
 
 export default Main
